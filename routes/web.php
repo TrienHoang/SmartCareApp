7aa3f246d148392b39cf5012841ff88be7517660
hieuk5 <hieulvph51615@gmail.com>
2025-05-30 11:37:17 +0700

    update giao diện đăng nhập

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,3 +17,5 @@ Route::post('/register', [AuthController::class, 'register'])->name('postRegiste
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware('auth')->name('dashboard');
+
+Route::get('admin/users', [UserController::class, 'index'])->name('admin.users.index');
