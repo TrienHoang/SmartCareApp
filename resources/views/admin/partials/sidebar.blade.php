1f6659ae8a75aa206a606973a5cd6c2f8f44b380
TrienHoang <trienhtph51464@gmail.com>
2025-06-02 16:30:11 +0700

    fix merge

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -32,7 +32,7 @@
 </svg>
 
 </span>
-      <span class="app-brand-text demo menu-text fw-bold ms-2">Frest</span>
+      <span class="app-brand-text demo menu-text fw-bold ms-2">SmartCare</span>
     </a>
 
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
