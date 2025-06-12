a5748326a2c03fe57d18ab4b74e152899362fd0b
nguyencuong2514 <cuongnxph50617@gmail.com>
2025-06-12 23:04:57 +0700

    Thêm ngày cho lịch làm việc

--- app/Http/Controllers/admin/SchedulesController.php
+++ app/Http/Controllers/admin/SchedulesController.php
@@ -29,6 +29,7 @@ class SchedulesController extends Controller
     {
         $request->validate([
             "doctor_id" => "required|exists:doctors,id",
+            'day' => 'required|date',
             'day_of_week' => 'required|string|max:255',
             'start_time' => 'required|date_format:H:i',
             'end_time' => 'required|date_format:H:i|after:start_time',
@@ -49,6 +50,7 @@ class SchedulesController extends Controller
     {
         $request->validate([
             "doctor_id" => "required|exists:doctors,id",
+            'day' => 'required|date',
             'day_of_week' => 'required|string|max:255',
             'start_time' => 'required|date_format:H:i',
             'end_time' => 'required|date_format:H:i|after:start_time',
