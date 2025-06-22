2de65cc81957f05cb1aee01e6392585bf4aae659
TrienHoang <trienhtph51464@gmail.com>
2025-06-22 23:43:11 +0700

    Fix thông báo toarjs

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,6 @@
             height: 100%;
             margin: 0;
             overflow: hidden;
-            /* ❗Không để body scroll */
         }
 
         .layout-wrapper {
@@ -64,27 +63,16 @@
     </style>
 
 </head>
-
 <body>
-    <!-- Google Tag Manager noscript -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DDHKGP" height="0" width="0"
-            style="display: none; visibility: hidden"></iframe>
-    </noscript>
-    <!-- End Google Tag Manager noscript -->
-
     <div class="layout-wrapper layout-content-navbar">
         <div class="layout-container">
             @include('admin.partials.sidebar')
-
             <div class="layout-page">
                 @include('admin.partials.navbar')
-
                 <div class="content-wrapper">
                     <div class="container-xxl flex-grow-1 container-p-y">
                         @yield('content')
                     </div>
-
                     @include('admin.partials.footer')
                 </div>
             </div>
@@ -92,42 +80,25 @@
     </div>
 
     @include('admin.partials.scripts')
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
     <script>
         $(document).ready(function() {
             @if (session('success'))
                 toastr.success("{{ session('success') }}");
             @endif
-
             @if (session('error'))
                 toastr.error("{{ session('error') }}");
             @endif
-
             @if ($errors->any())
                 @foreach ($errors->all() as $error)
                     toastr.error("{{ $error }}");
                 @endforeach
             @endif
-
             @if (session('date_swapped'))
-                toastr.warning("Ngày bắt đầu lớn hơn ngày kết thúc. Hệ thống đã tự động hoán đổi giúp bạn.",
-                    "Cảnh báo");
+                toastr.warning("Ngày bắt đầu lớn hơn ngày kết thúc. Hệ thống đã tự động hoán đổi giúp bạn.", "Cảnh báo");
             @endif
         });
-
     </script>
     @stack('scripts')
-
     @yield('scripts')
-
-
 </body>
-
 </html>
