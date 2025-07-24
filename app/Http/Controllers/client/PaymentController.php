bf34c577129df7456ffcb3a713a833ff13ec6535
nguyencuong2514 <cuongnxph50617@gmail.com>
2025-07-25 06:59:40 +0700

    Quản lý ca làm việc và đăng ký ca làm bác sĩ đổi trạng thái đơn đăng ký ca làm

--- app/Http/Controllers/client/PaymentController.php
+++ app/Http/Controllers/client/PaymentController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
-use App\Http\Controllers\Controller;
 
 class PaymentController extends Controller
 {
