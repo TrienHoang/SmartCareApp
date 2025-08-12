1b7d41321c5be780918a56311b7cc4e87a491809
hieuk5 <hieulvph51615@gmail.com>
2025-08-13 00:03:30 +0700

    bổ sung triệu chứng lịch hẹn

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -20,6 +20,7 @@ class Appointment extends Model
         'status',
         'reason',
         'cancel_reason',
+        'symptom_note',
         'treatment_plan_id',
         'created_at',
         'updated_at',
