145e20c55b0a96895b9d74bc73481ecdfe8b7c28
TrienHoang <trienhtph51464@gmail.com>
2025-08-20 00:10:29 +0700

    pull form dev

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'SmartCare - Hệ thống Y tế')</title>
-
+    <link rel="icon" href="{{ asset('path/to/your-icon.png') }}" type="image/png">
     <!-- Google Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap">
 
